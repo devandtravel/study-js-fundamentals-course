3774e2bf970a63b1f21b77311fc63872dba66413
devandtravel <devandtravel@gmail.com>
2021-08-28 15:11:42 +0300

    add: while

---
+++ theory/05-loops/01-while.js
@@ -0,0 +1,11 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let index = 0
+
+while (index < numbers.length) {
+  console.log('While loop')
+  console.log(numbers[index])
+  index++
+}
+
+console.log('After while loop')

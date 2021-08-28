b542abecfc91990d3da949bb238a2691a2cf7b05
devandtravel <devandtravel@gmail.com>
2021-08-28 15:30:02 +0300

    add: exercise6

--- theory/05-loops/01-while.js
+++ theory/05-loops/01-while.js
@@ -1,11 +1,11 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-let i = 0
+let index = 0
 
-while (i < numbers.length) {
+while (index < numbers.length) {
   console.log('While loop')
-  console.log(numbers[i])
-  i++
+  console.log(numbers[index])
+  index++
 }
 
 console.log('After while loop')

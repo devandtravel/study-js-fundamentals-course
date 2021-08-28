41eedd5e61e7d452901036417a2bf7792babc1bf
devandtravel <devandtravel@gmail.com>
2021-08-28 15:19:26 +0300

    add: excersize5

--- theory/05-loops/01-while.js
+++ theory/05-loops/01-while.js
@@ -1,11 +1,11 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-let index = 0
+let i = 0
 
-while (index < numbers.length) {
+while (i < numbers.length) {
   console.log('While loop')
-  console.log(numbers[index])
-  index++
+  console.log(numbers[i])
+  i++
 }
 
 console.log('After while loop')
---
+++ theory/05-loops/02-for.js
@@ -0,0 +1,10 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let index = 0
+
+for (let index = 0; index < numbers.length; index++) {
+  const element = numbers[index]
+  console.log(element)
+}
+
+console.log('After for loop')

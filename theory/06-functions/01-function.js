bac12882466d261384175d7ad6ae61f7a64b49eb
devandtravel <devandtravel@gmail.com>
2021-08-30 14:51:57 +0300

    add: function, Math, exersize8

---
+++ theory/06-functions/01-function.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+  console.log('hi from sum')
+  return a + b
+}
+const s = sum(1, 2)
+console.log(s)
---
+++ theory/07-Math/01-Math.js
@@ -0,0 +1,6 @@
+console.log(Math.PI) //3.141592653589793
+console.log(Math.random()) // (0:1)
+console.log(Math.floor(0.6346296429945357)) // 0
+console.log(Math.ceil(0.6346296429945357)) // 1
+console.log(Math.round(0.6346296429945357)) // (Math.floor(n):Math.ceil(n)) by math rules
+

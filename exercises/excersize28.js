b7276fd043ef8cd7932bc1191d3198b25caf1637
devandtravel <devandtravel@gmail.com>
2021-09-23 21:55:51 +0300

    add: excersize28

---
+++ exercises/excersize28.js
@@ -0,0 +1,12 @@
+// Практика на деструктуризацию массивов
+// Дана функция getInfo, возвращающая массив в результате вызова.
+// Создайте переменные carName и carSeries и при помощи деструктуризации назначьте ей значения, возвращаемые функцией
+
+function getInfo() {
+  return ['BMW', 'X3']
+}
+
+const [carName] = getInfo()
+const [, carSeries] = getInfo()
+
+console.log(carName, carSeries)

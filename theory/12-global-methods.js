f0fdf25620f6409f4cd64c9b1b0e377c2d3ee7f9
devandtravel <devandtravel@gmail.com>
2021-09-24 14:16:54 +0300

    global-methods

---
+++ theory/12-global-methods.js
@@ -0,0 +1,20 @@
+Number('12') // 12
+Number({}) // NaN
+Number(true) // 1
+
+String(000) // '0'
+String(123) // '123'
+
+Boolean('123') // true
+Boolean('') // false
+Boolean([]) // true
+
+const obj = { a: 1, b: 2, c: 3 }
+Object.create({}) // {}
+Object.keys(obj) // [ 'a', 'b', 'c' ]
+Object.values(obj) // [ 1, 2, 3 ]
+Object.entries(obj) // [ [ 'a', 1 ], [ 'b', 2 ], [ 'c', 3 ] ]
+
+Array.isArray({}) // false
+Array.from('string') // [ 's', 't', 'r', 'i', 'n', 'g' ]
+Array.of([1, 2, 3], x => x * 2) // [ 2, 4, 6 ]

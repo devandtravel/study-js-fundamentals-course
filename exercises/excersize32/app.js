9ca6f565704b05816979d7aa6e080f2d37732006
devandtravel <devandtravel@gmail.com>
2021-12-28 13:36:07 +0300

    01-querySelector, excersize32

---
+++ exercises/excersize32/app.js
@@ -0,0 +1,4 @@
+const title = document.querySelector('.gray')
+const btn = document.getElementById('btn')
+const subtitle = document.getElementsByClassName('title gray')[0]
+const sections = document.querySelectorAll('section')
---
+++ theory/01-querySelector.js
@@ -0,0 +1,5 @@
+const h1 = document.querySelector('h1') // <h1>...</h1>
+const h1All = document.querySelectorAll('h1') // [<h1>...</h1>, ...]
+const h2 = document.getElementById('id') // <h2 id="id">...</h2>
+const h3 = document.getElementsByClassName('class') // <h3 class="class">...</h3>
+const h4 = document.getElementsByTagName('h4') // <h4>...</h4>
